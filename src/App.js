731305c1c314196e03d2c75fe6cc119d9724ef92
Bianca Hale <sashabtt@gmail.com>
2018-06-11 17:08:09 -0600

    got form up;not working

--- src/App.js
+++ src/App.js
@@ -2,15 +2,27 @@
 // Ability to edit a flash card 
 // Ability to delete a flash card
 // Ability to show the flash card answer
-
-
-
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 class App extends Component {
-  state = {cards: [], question: 'Here is a question', answer: 'This is the answer'}
+  state = {cards: [
+    {id: 1, question: 'Here is a question', answer: 'This is the answer'},
+    {id: 2, question: 'Here is a question', answer: 'This is the answer'},
+    {id: 3, question: 'Here is a question', answer: 'This is the answer'},
+  ]}
+
+  handleAddCard = (e) => {
+    e.preventDefault()
+    this.props.addCard(this.state.cards)
+    this.setState({ cards: [] })
+  }
+
+  handleChange = (e) => {
+    this.setState({ cards: e.target.value })
+  }
 
   addCard = () => {
     this.setState(state =>{
@@ -26,9 +38,6 @@ class App extends Component {
   //   })
   // }
 
-
-
-
   render() {
     return (
       <div className="App">
@@ -36,10 +45,22 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Flash Cards</h1>
         </header>
-        <h1>
-          Start Studying Here
-        </h1>
-        <button onClick={this.addCard}>Add A Card</button>
+      <h1>
+        Start Studying Here
+      </h1>
+      <form onSubmit={this.addCard}>
+        <input 
+          value={this.state.cards}
+          onChange={this.handleChange}
+          placeholder="Add a Question" 
+        />
+        <input 
+          value={this.state.cards}
+          onChange={this.handleChange}
+          placeholder="Add an Answer" 
+        />
+        <button onClick={this.addCard}>Add Card</button>
+      </form>
         {/* <button onClick={this.addFront}>Add The Answer</button> */}
       </div>
     );
@@ -47,3 +68,20 @@ class App extends Component {
 }
 
 export default App;
+
+{/* <form onSubmit={this.handleSubmit}>
+        <input 
+          value={this.state.name}
+          onChange={this.handleChange}
+          required 
+          placeholder="Add An Item" 
+        />
+      </form> */}
+
+      // class App extends Component {
+      //   state = {
+      //     todos: [
+      //       { id: 1, name: 'Learn Rails', complete: true },
+      //       { id: 2, name: 'Learn React', complete: false },
+      //       { id: 3, name: 'Learn Redux', complete: false },
+      //     ]}
